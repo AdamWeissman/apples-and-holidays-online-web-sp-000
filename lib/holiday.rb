0a374d87ce584beef65fc6eeeb80cfa286a1984d
Adam Weissman <adam@comedytragedyepic.com>
2019-09-20 10:54:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_names = []
-  holiday_hash.each do |seasons, holiday|
+  holiday_hash.each do |season, holiday|
     holiday_names << season[holiday] if season[holiday].values.include? "BBQ"
   end
   return holiday_names
